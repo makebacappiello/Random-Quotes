2e07cd6fe46c9cd3e1aa34f28bb12f647b610314
makebacappiello <makebacappiello@gmail.com>
2026-09-08 20:13:54 -0400

    First Try

--- js/script.js
+++ js/script.js
@@ -17,6 +17,7 @@ const quotes = [
       "A hug is a wonderful thing. It makes you feel wanted, needed, and loved all at the same time.",
     author: "Gregory Garcia and Dan Coscino",
     date: "October 26, 2010",
+    citation: "A Hug Is a Wonderful Thing. ",
   },
   {
     quote: "Believe you can and you're halfway there.",
@@ -28,7 +29,7 @@ const quotes = [
   },
   {
     quote:
-      "It is better wither to be silent, or to say things of more value than silence. Sooner throw a pearl at hazard than an idle or useless word; and do not say a little in many words, but a great deal in a few.",
+      "It is better with either to be silent, or to say things of more value than silence. Sooner throw a pearl at hazard than an idle or useless word; and do not say a little in many words, but a great deal in a few.",
     author: "Pythagoras",
     date: "582 BC - 507 BC",
   },
@@ -37,12 +38,14 @@ const quotes = [
       "If the only tool you have is a hammer, you tend to see every problem as a nail.",
     author: "Abraham Maslow",
     date: "1908 - 1970",
+    citation: "Toward a Psychology of Being.",
   },
   {
     quote:
       "Take the attitude of a student, never be too big to ask questions, never know too much to learn something new.",
     author: "Og Mandino",
     date: "1923 - 1996",
+    citation: "The Greatest Salesman in the World.",
   },
   {
     quote: " There are too many people, and too few human beings.",
@@ -53,17 +56,35 @@ const quotes = [
 /***
  * `getRandomQuote` function
  ***/
+let currentQuote = " ";
+//this holds the empty string to be chosen later for the array
 
 function getRandomQuote() {
-  const getOneQuote = () => {
+  // the function is using the math function to get a random quote from the variable get one quote from the index which will be generated from the variable randomIndex
+  let getOneQuote = () => {
     const randomIndex = Math.floor(Math.random() * quotes.length);
-    currentQuote(quotes[randomIndex]);
+    currentQuote = quotes[randomIndex];
   };
+  getOneQuote();
+  //calling the function that gets the random quote
 }
+//calling the function that gets everything after calculation
+getRandomQuote();
 
 /***
- * `printQuote` function
+//  * `printQuote` function
  ***/
+
+function printQuote() {
+  getRandomQuote();
+  let quoteBox = " ";
+  quoteBox += `<p> ${currentQuote.quote} </p>
+        <p>${currentQuote.author} </p>
+        // <p>${currentQuote.citation}</>
+          <span >${currentQuote.date}</span>`;
+
+  document.getElementById("quote-box").innerHTML = quoteBox;
+}
 
 /***
  * click event listener for the print quote button
